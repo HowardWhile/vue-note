52c6101da16591eabcf73d49902b884faa22aefd
HOW-DELL <makubex49@gmail.com>
2021-11-03 14:45:56 +0800

    day20

--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,17 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    numbers: [1,3,5,7,9]
   },
   mutations: {
+    ADD_NUMBER(state,payload) {
+      state.numbers.push(payload)
+    }
   },
   actions: {
+    addNumber(context,number) {
+      context.commit("ADD_NUMBER", number)
+    }
   },
   modules: {
   }
