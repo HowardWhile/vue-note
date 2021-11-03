ecae431b38c5fadac01a5eff4550694902e4ab19
HOW-DELL <makubex49@gmail.com>
2021-11-03 15:11:10 +0800

    day-23 利用netlify部屬你的vue專案

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="你學會了嗎? 留言告訴我吧!"/>
   </div>
 </template>
 
